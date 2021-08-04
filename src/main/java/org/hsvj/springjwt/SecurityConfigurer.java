b3a7425df3ae52356e0168b07038d5247f077a99
vijayanandr <vijayanandr@vmware.com>
2021-08-04 12:17:46 +0530

    integration with UI

--- src/main/java/org/hsvj/springjwt/SecurityConfigurer.java
+++ src/main/java/org/hsvj/springjwt/SecurityConfigurer.java
@@ -3,7 +3,9 @@ package org.hsvj.springjwt;
 import org.hsvj.springjwt.filter.JwtRequestFilter;
 import org.hsvj.springjwt.service.JwtUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +13,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -18,6 +26,10 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
     private JwtUserDetailsService jwtUserDetailsService;
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
+    @Autowired
+    private Environment environment;
+
+    private static final String UI_CLIENT_URL_PROPERTY = "ui.client.url";
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -32,6 +44,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/authenticate/**").permitAll()
@@ -40,5 +53,19 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList(environment.getProperty(UI_CLIENT_URL_PROPERTY)));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "content-type", "Cache-Control"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "content-type", "Cache-Control"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
